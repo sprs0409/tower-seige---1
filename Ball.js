9b05a6c564d256b805bfb5d4db22acd28fbe7183
sprs0409 <79050980+sprs0409@users.noreply.github.com>
2021-02-27 11:04:17 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,34 @@
+class Ball{
+
+
+constructor(x,y,width,height,angle){
+var options={
+density:1
+
+
+
+
+}
+this.body=Bodies.rectangle(x,y,width,height,options);
+this.width = width;
+this.height = height;
+World.add(world, this.body);
+}
+display(){
+var pos=this.body.position;
+var angle=this.body.angle;
+
+push();
+translate(pos.x, pos.y);
+rotate(angle);
+rectMode(CENTER);
+strokeWeight(4);
+stroke("green");
+fill(255);
+rect(0, 0, this.width, this.height);
+pop();
+
+}
+
+
+}
